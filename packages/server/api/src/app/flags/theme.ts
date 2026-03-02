8234db458476981eaa1db965128ba3e2dc101bb5
sdqwoo <bwjoung12@gmail.com>
2026-03-02 22:02:25 +0900

    Change shinhan Logo

--- packages/server/api/src/app/flags/theme.ts
+++ packages/server/api/src/app/flags/theme.ts
@@ -64,9 +64,9 @@ export function generateTheme({
 }
 
 export const defaultTheme = generateTheme({
-    primaryColor: '#6e41e2',
-    websiteName: 'Activepieces',
-    fullLogoUrl: 'https://cdn.activepieces.com/brand/full-logo.png',
-    favIconUrl: 'https://cdn.activepieces.com/brand/favicon.ico',
-    logoIconUrl: 'https://cdn.activepieces.com/brand/logo.svg',
+    primaryColor: '#083DC9',
+    websiteName: '신한 AX Station',
+    fullLogoUrl: '/src/assets/shinhan/shinhan-logo.png',
+    favIconUrl: '/src/assets/shinhan/shinhanicon.ico',
+    logoIconUrl: '/src/assets/shinhan/shinhanlogo.svg',
 })
